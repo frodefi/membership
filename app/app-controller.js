ffdc0e56ecd24f6aa28ff7b371e666c0679d8466
Frode Fikke <frodefi@gmail.com>
2014-10-26 03:08:38 +0200

    Brushed up the query field and added help-text

--- app/app-controller.js
+++ app/app-controller.js
@@ -2,6 +2,10 @@ angular.module('app.controllers')
   .controller('AppCtrl', ['$scope','userService','dataService','$routeSegment',
     function ($scope, userService, dataService, $routeSegment) {
       $scope.model = {};
+      $scope.model.query = "";
+      $scope.model.sortField = "";
+      $scope.model.reverse = false;
+      $scope.model.showHelpQuery = false;
       $scope.model.data = dataService;
       $scope.model.user = userService;
     }]);
